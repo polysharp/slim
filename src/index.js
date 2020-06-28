f5827535c841e8cffa275e71c2f54a6861045a61
Henri Fardeau <henri.fardeau@outlook.fr>
2020-06-28 16:34:28 +0200

    fix: index import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/export */
 
 export { default as GlobalStyle } from './core/GlobalStyle';
-export * from 'core/GlobalStyle';
+export * from './core/GlobalStyle';
 
 export { default as Grid } from './core/Grid';
-export * from 'core/Grid';
+export * from './core/Grid';
