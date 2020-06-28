c89bdad7970b853276a913fda3b1d1b2f011c1be
Henri Fardeau <henri.fardeau@outlook.fr>
2020-06-28 22:37:29 +0200

    fix: publish problem

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-/* eslint-disable import/export */
-
 export { default as GlobalStyle } from './core/GlobalStyle';
-export * from './core/GlobalStyle';
+// export * from './core/GlobalStyle';
 
 export { default as Grid } from './core/Grid';
-export * from './core/Grid';
+// export * from './core/Grid';
 
 export { default as Tile } from './core/Tile';
-export * from './core/Tile';
+// export * from './core/Tile';
 
 export { default as Hidden } from './core/Hidden';
-export * from './core/Hidden';
+// export * from './core/Hidden';
